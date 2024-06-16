1882ab4945d1b75a99e7e7a8ab0be3b57728b34f
Istrate Cristofor Stefan <istratecristoforstefan@gmail.com>
2024-06-16 13:57:51 +0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PostsList from "./features/post/PostsList";
+import AddPostForm from "./features/post/AddPostForm";
+
+function App() {
+
+  return (
+    <>
+        <PostsList />
+        <AddPostForm />
+    </>
+  )
+}
+
+export default App
